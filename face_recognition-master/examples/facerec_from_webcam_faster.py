a3e0fc6d7d891457d6a9ec897203ff6979b1e3df
SiiHyunn <popo2546@naver.com>
2022-06-27 15:34:10 +0900

    220627

--- face_recognition-master/examples/facerec_from_webcam_faster.py
+++ face_recognition-master/examples/facerec_from_webcam_faster.py
@@ -28,19 +28,19 @@ obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 biden_image = face_recognition.load_image_file("biden.jpg")
 biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
-sihyun_image = face_recognition.load_image_file("sihyun.jpg")
-sihyun_face_encoding = face_recognition.face_encodings(sihyun_image)[0]
+# sihyun_image = face_recognition.load_image_file("sihyun.jpg")
+# sihyun_face_encoding = face_recognition.face_encodings(sihyun_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
     obama_face_encoding,
-    biden_face_encoding,
-    sihyun_face_encoding
+    biden_face_encoding
+    # sihyun_face_encoding
 ]
 known_face_names = [
     "Barack Obama",
-    "Joe Biden",
-    "Si Hyun"
+    "Joe Biden"
+    # "Si Hyun"
 ]
 
 # Initialize some variables
